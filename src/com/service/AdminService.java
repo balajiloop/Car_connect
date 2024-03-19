b5ea211b9869581ef3d81a12ac299043e5dd4067
balajiloop <81860811+balajiloop@users.noreply.github.com>
2024-03-19 14:05:01 +0530

    Update AdminService.java

--- src/com/service/AdminService.java
+++ src/com/service/AdminService.java
@@ -11,57 +11,5 @@ import com.model.Admin;
 
 public class AdminService {
 
-	AdminDao adminDao = new AdmindaoImpl();
-
-	public List<Admin> fetchAllAdmin() throws SQLException, DatabaseConnectionException 
-	{
-		List<Admin> list = adminDao.fetchAllAdmin();
-		return list;
-	}
-
-	public Admin getAdminById(List<Admin> list, int id) throws InvalidInputException 
-	{
-		for (Admin ad: list) {
-			if (ad.getId() == id)
-				return ad;
-		}
-		throw new InvalidInputException("Admin not found :(\nRecheck the admin Id");
-	}
-
-	public Admin getAdminByUsername(List<Admin> list, String name) throws InvalidInputException
-	{
-		for (Admin ad : list) 
-		{
-			if (ad.getUsername().equals(name))
-				return ad;
-		}
-		throw new InvalidInputException("Incorrect Username :<");
-	}
-
-	public void createAdmin(String firstName, String lastName, String email, String phoneNumber,
-			String username, String password, String role,String joiningdate)
-			throws DatabaseConnectionException, SQLException 
-	{
-
-		adminDao.createAdmin(firstName, lastName, email, phoneNumber, username, password,role, joiningdate);
-
-	}
-
-	public void updateAdmin(int id, String field, String newVal) throws DatabaseConnectionException, SQLException, InvalidInputException 
-	{
-		String fieldd=field.toLowerCase().replace(" ","_");
-		if(fieldd.equals("id"))
-			throw new InvalidInputException("Sorry!! Admin Id could not be updated :<");
-		else
-			adminDao.updateAdmin(id,fieldd,newVal);
-		
-	}
-
-	public void deleteAdminById(int id) throws SQLException, DatabaseConnectionException 
-	{
-		
-		adminDao.deleteAdminById(id);
-		
-	}
-
+	
 }
