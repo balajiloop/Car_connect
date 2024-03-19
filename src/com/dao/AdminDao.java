fee4ab1f75df64066cc238e2c226d3bd3741da1a
balajiloop <81860811+balajiloop@users.noreply.github.com>
2024-03-19 14:03:51 +0530

    Update AdminDao.java

--- src/com/dao/AdminDao.java
+++ src/com/dao/AdminDao.java
@@ -9,13 +9,6 @@ import com.model.Admin;
 
 public interface AdminDao {
 
-	List<Admin> fetchAllAdmin() throws SQLException, DatabaseConnectionException;
-
-	void createAdmin(String firstName, String lastName, String email,String phoneNumber,  String username,
-			String password, String role,String joiningdate) throws DatabaseConnectionException, SQLException;
-
-	void updateAdmin(int id, String field, String newVal) throws DatabaseConnectionException, SQLException;
-
-	void deleteAdminById(int id) throws SQLException, DatabaseConnectionException;
+	
 
 }
